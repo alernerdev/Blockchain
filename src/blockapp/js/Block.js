78bb83d76cc457c3415a763db34911c7363e236d
Alex Lerner <alernerdev@gmail.com>
2018-03-18 13:45:50 -0400

    implemented hash generation depending on the block, nonce, and data values.  Still buggy

--- src/blockapp/js/Block.js
+++ src/blockapp/js/Block.js
@@ -1,44 +1,100 @@
 import React from 'react';
-import { TextArea, Input, Container, Label, Table} from 'semantic-ui-react';
+import { TextArea, Input, Container, Label, Table, Button} from 'semantic-ui-react';
 import * as CryptoJS from 'crypto-js';
 
-export default class Hash extends React.Component {
+export default class Block extends React.Component {
+
 	state = {
 		data: '',
+		block: '1',
+		nonce: '',
 		encoded: ''
 	};
 
 	constructor() {
 		super();
+
+		this.difficulty = 4;        // number of zeros required at front of hash
+		this.maximumNonce = 500000; // limit the nonce to this so we don't mine too long
+
+		// number of leading zeroes in the hash we need to generate
+		this.pattern = '';
+		for (var x=0; x < this.difficulty; x++) {
+			this.pattern += '0';
+		}
 	}
 
 	componentDidMount() {
-		let hash = CryptoJS.SHA256(this.state.data).toString(CryptoJS.enc.Hex);
-		this.setState({
-			encoded: hash
-		});
+		this.doHash();
+	}
+
+	doHash = () => {
+		let message = this.makeMessage();
+		let hash = CryptoJS.SHA256(message).toString(CryptoJS.enc.Hex);
+		this.setState({	encoded: hash});
 	}
 
 	onDataChange = (e) => {
-		let data = e.target.value;
-		let hash = CryptoJS.SHA256(data).toString(CryptoJS.enc.Hex);
-
-		this.setState({
-			data: data,
-			encoded: hash
-		});
+		this.setState({data: e.target.value});
+		this.doHash();
 	};
+
+	onBlockNumberChange = (e) => {
+		this.setState({block: e.target.value});
+		this.doHash();
+	};
+
+	onNonceChange = (e) => {
+		this.setState({nonce: e.target.value});
+		this.doHash();
+	};
+
+	// the total message consists of user data, but also block number and nonce
+	makeMessage = ()  => {
+		// if this was a blockchain, the hash of the previous block would be included as well
+		return `${this.state.block}${this.state.nonce}${this.state.data}`;
+	}
+
+	handleMineClick = () => {
+		console.log("mine click");
+	}
 
 	render() {
 		const style = {width : "100%"};
 
 		return(
 			<Container>
-				<Table>
+				<Table basic='very'>
 					<Table.Body>
+					<Table.Row verticalAlign='top'>
+							<Table.Cell textAlign='right'>
+								<Label basic>Block</Label>
+							</Table.Cell>
+
+							<Table.Cell>
+								<Input fluid
+									type='text'
+									onChange={this.onBlockNumberChange}
+								/>
+							</Table.Cell>
+						</Table.Row>
+
 						<Table.Row verticalAlign='top'>
 							<Table.Cell textAlign='right'>
-								<Label>Data:</Label>
+								<Label basic>Nonce:</Label>
+							</Table.Cell>
+
+							<Table.Cell>
+								<Input fluid
+									type='text'
+									onChange={this.onNonceChange}
+								/>
+							</Table.Cell>
+						</Table.Row>
+
+						<Table.Row verticalAlign='top'>
+							<Table.Cell textAlign='right'>
+								<Label basic>Data:</Label>
 							</Table.Cell>
 
 							<Table.Cell>
@@ -53,7 +109,7 @@ export default class Hash extends React.Component {
 
 						<Table.Row verticalAlign='top'>
 							<Table.Cell textAlign='right'>
-								<Label>Hash:</Label>
+								<Label basic>Hash:</Label>
 							</Table.Cell>
 							<Table.Cell>
 								<Input fluid
@@ -61,6 +117,15 @@ export default class Hash extends React.Component {
 									value={this.state.encoded}
 									readOnly
 								/>
+							</Table.Cell>
+						</Table.Row>
+						<Table.Row verticalAlign='top'>
+							<Table.Cell textAlign='right'>
+							</Table.Cell>
+							<Table.Cell>
+								<Button primary
+									onClick={this.handleMineClick}
+								>Mine</Button>
 							</Table.Cell>
 						</Table.Row>
 					</Table.Body>
